4e656a786549dae211bad6b9a027ebbd2c766dda
wuhonglei <1017368065@qq.com>
2020-12-13 15:09:31 +0800

    所有页面共用一个 skeleton

--- README.md
+++ README.md
@@ -1,24 +1,7 @@
 # skeleton
+该项目介绍了骨架屏的使用案例，原理：
+1.编写骨架屏组件（div、svg组合等）
+2.将组件转换为css和dom节点插入到 index.html 中 #app 的挂载点
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+场景一、所有页面公用一个 skeleton
+场景二、不同路由页面使用不同的 skeleton
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const SkeletonWebpackPlugin = require('vue-skeleton-webpack-plugin');
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new SkeletonWebpackPlugin({
+                webpackConfig: {
+                    entry: {
+                        app: path.join(__dirname, './src/skeleton.js'),
+                    },
+                },
+                minimize: true,
+                quiet: true,
+            }),
+        ]
+    }
+}
